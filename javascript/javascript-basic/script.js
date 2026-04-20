0ea8b67b3e57552364767528625fac8c6d741bd5
ilhamabi <33386487+ilhamabi@users.noreply.github.com>
2026-04-20 23:42:55 +0700

    Add javascript basic result

---
+++ javascript/javascript-basic/script.js
@@ -0,0 +1,44 @@
+var name="Ilham Abi"
+let age=25
+const country="Indonesia"
+
+console.log(name)
+console.log(age)
+console.log(country)
+
+let number=10
+let string="ini string"
+let boolean=true
+let array=[1,2,3]
+let object={name:"Ilham", age:25, country:"Indonesia"}
+
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof array)
+console.log(typeof object)
+
+const x=10
+const y=20
+
+let numAdd=x+y
+let numSub=x-y
+let numMul=x*y
+let numDiv=x/y
+
+console.log(numAdd)
+console.log(numSub)
+console.log(numMul)
+console.log(numDiv)
+
+let isGreater = x === y;
+let isLess = x < y;
+let isEqual = x === y;
+
+console.log(isGreater);
+console.log(isLess);
+console.log(isEqual);
+
+let checkAnd = (x < 15) && (y > 15);
+
+console.log(checkAnd);
